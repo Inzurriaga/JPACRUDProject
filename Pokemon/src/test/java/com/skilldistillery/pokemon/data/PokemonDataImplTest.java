a78d0d527955c2d2f789eed7dfaaf22804c51411
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2020-01-20 08:19:51 -0700

    readme

--- Pokemon/src/test/java/com/skilldistillery/pokemon/data/PokemonDataImplTest.java
+++ Pokemon/src/test/java/com/skilldistillery/pokemon/data/PokemonDataImplTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.skilldistillery.pokemon.entities.Pokemon;
 
 class PokemonDataImplTest {
 	private PokemonData dao;
@@ -22,8 +25,47 @@ class PokemonDataImplTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Retrieve Bulbasaur from id 1")
+	void test1() {
+		Pokemon pokemon = dao.findById(1);
+		assertEquals(1, pokemon.getId());
+		assertEquals("Bulbasaur", pokemon.getName());
+	}
+	
+	@Test
+	@DisplayName("Create A new Pokemon to the DB and retrieve its PK")
+	void test2() {
+		Pokemon pokemon = new Pokemon();
+		pokemon.setPokedexNumber(10);
+		pokemon.setName("bob");
+		pokemon.setDescription("a guy name bob");
+		pokemon.setHeightInches(10.0);
+		pokemon.setWeightPounds(10.0);
+		pokemon.setType("fire");
+		Pokemon createdPoke = dao.create(pokemon);
+		assertNotEquals(0, createdPoke.getId());
+	}
+	
+	@Test
+	@DisplayName("Update Pokemon from database")
+	void test3() {
+		Pokemon pokemon = new Pokemon();
+		pokemon.setId(10);
+		pokemon.setPokedexNumber(10);
+		pokemon.setName("bob");
+		pokemon.setDescription("a guy name bob");
+		pokemon.setHeightInches(10.0);
+		pokemon.setWeightPounds(10.0);
+		pokemon.setType("fire");
+		boolean updated = dao.update(pokemon);
+		assertTrue(updated);
+	}
+	
+	@Test
+	@DisplayName("Delete Pokemon from database")
+	void test4() {
+		boolean deleted = dao.delete(10);
+		assertTrue(deleted);
 	}
 
 }
---
+++ README.md
@@ -0,0 +1,36 @@
+## Film Query Project
+### Week Seven Homework for Skill Distillery
+
+#### Overview
+
+A web application made with java and spring framework. the application is base off of pokemon but the user is allow to create the pokemon they want. not only the user can create pokemon but they can also update the pokemon any pokemon in the database or even delete any pokemon. The info the user can see and add to the pokemon is the pokemon name, the pokemon description, the pokemon height (inches), the pokemon weigght (pounds), and the pokemon type.
+
+
+#### Concepts
+
+* methods
+* list
+* arrays
+* class
+* objects
+* loops
+* SQL
+* CRUD
+
+
+#### Technologies Used
+
+ * Java
+ * MySQL
+ * Spring Boot
+ * javascript
+
+ #### Future
+
+ * fix the css to make the content more dynamic and mobile friendly
+ * add the feature to add the pokemons weakness
+ * 
+
+#### Lessons Learned
+
+learn to create a CRUD application using Java as the main language and Spring as the framework.
